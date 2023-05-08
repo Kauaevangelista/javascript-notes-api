ee14a2542273e505e629a46411c939ef4474adbb
Kauaevangelista <kaua13evangelista@gmail.com>
2023-05-08 09:38:15 -0300

    Senhas com Hashford

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import connectToMongoDB from './db'
+import router from './routes/users'
 
 // import usersRouter from './routes/users'
 
@@ -11,6 +12,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/users', router)
 
 const PORT = process.env.PORT || 3000
 
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcrypt'
 
 
 const userSchema = new mongoose.Schema({
@@ -8,5 +9,20 @@ const userSchema = new mongoose.Schema({
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
   });
+
+  userSchema.pre('save', function (next) {
+    if (this.isNew || this.isModified('password')) {
+        bcrypt.hash(this.password, 10,
+           (err: mongoose.CallbackError | undefined, hashedPassword: string) => {
+            if (err)
+             next(err);
+            else {
+             this.password = hashedPassword;
+             next();
+            }
+          });
+      } else {
+      }
+  })
 
   export default mongoose.model('User', userSchema)
